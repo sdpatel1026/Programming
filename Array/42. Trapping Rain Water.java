8f5082acc915425707498afbd52086c2a346af19
sdpatel1026 <59508327+sdpatel1026@users.noreply.github.com>
2021-10-16 10:42:37 +0530

    Create 42. Trapping Rain Water.java

---
+++ Array/42. Trapping Rain Water.java
@@ -0,0 +1,29 @@
+
+//https://leetcode.com/problems/trapping-rain-water/
+class Solution {
+    public int trap(int[] height) {
+        
+        int len = height.length;
+        
+        if(len<3)
+            return 0;
+       
+        int leftMax[]= new int[len];
+        int rightMax[] = new int[len];
+        leftMax[0] = height[0];
+        for(int i=1;i<len;i++)
+            leftMax[i] = Math.max(height[i],leftMax[i-1]);
+        rightMax[len-1] = height[len-1];
+        for(int i = len-2;i>=0;i--)
+            rightMax[i]=Math.max(height[i],rightMax[i+1]);
+        long count =0;
+        for(int i=1;i<len-1;i++)
+            count+=Math.min(leftMax[i],rightMax[i])-height[i];
+        
+        return (int)count;
+        
+    }
+}
+
+//TC:-O(n)
+//SC:-O(n)  
